d2c1baa9934ca663d0c7fcad1adcbc2101e88c5f
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2023-10-21 11:56:06 +0500

    Navbar layout change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,16 +2,26 @@ import Link from "next/link";
 import { RiBugFill } from "react-icons/ri";
 
 import NavLinks from "./NavLinks";
+import { Container, Flex } from "@radix-ui/themes";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center gap-6 h-14 px-5 border-b">
-      <Link href="/">
-        <RiBugFill className="text-2xl cursor-pointer" />
-      </Link>
-      <ul className="flex gap-6">
-        <NavLinks />
-      </ul>
+    <nav className="p-5 border-b">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="5">
+            <Link href="/">
+              <RiBugFill className="text-2xl cursor-pointer" />
+            </Link>
+            <ul className="flex gap-5">
+              <NavLinks />
+            </ul>
+          </Flex>
+          <Flex>
+            <Link href="/api/auth/signin">Sign in</Link>
+          </Flex>
+        </Flex>
+      </Container>
     </nav>
   );
 };
