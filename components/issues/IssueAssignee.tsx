4d20e23e2bb454716a33104b27cdd343119c8851
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2023-10-22 11:41:20 +0500

    Fetch users with react-query

--- components/issues/IssueAssignee.tsx
+++ components/issues/IssueAssignee.tsx
@@ -2,24 +2,32 @@
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import Skeleton from "react-loading-skeleton";
 
 const IssueAssignee = () => {
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
       const res = await axios.get<User[]>("/api/users");
-      setUsers(res.data);
-    };
-    fetchUsers();
-  }, []);
+      return res.data;
+    },
+    staleTime: 1000 * 60 * 5,
+  });
+
+  if (isLoading) return <Skeleton height="2.25rem" />;
+
+  if (error) return <Select.Root>Error</Select.Root>;
 
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign ..." />
       <Select.Content>
-        {users.map((user) => (
+        {users?.map((user) => (
           <Select.Item value={user.id} key={user.id}>
             {user.name}
           </Select.Item>
