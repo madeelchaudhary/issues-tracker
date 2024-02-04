70836a82e7f940e2b42be025c7e4dde98140b3de
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2024-02-04 16:09:00 +0500

    Use noStore to fix cache issue

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,10 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import { unstable_noStore as noStore } from "next/cache";
 
 export const GET = async (req: NextRequest) => {
+  noStore();
+
   const users = await prisma.user.findMany({
     orderBy: {
       name: "asc",
